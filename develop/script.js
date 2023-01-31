007be00a705259a7a632f242987fbda419e7d5fa
Brandon Guerrero <brandonguerrero@Brandons-MacBook-Air.local>
2023-01-30 22:23:00 -0800

    finished HTML, intitial JS

--- develop/script.js
+++ develop/script.js
@@ -2,6 +2,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var currentTime = dayjs();
+var dateEl = document.getElementById("currentDay")
+dateEl.textContent = currentTime.format('MMM DD, YYYY');
+
+for (var i=0;i<11;i++)
+}
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +28,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
